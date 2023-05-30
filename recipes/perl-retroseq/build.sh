995b70d5bbb75df2d095e318c8b0fcd29ccd1cfe
Ramprasad Neethiraj <20065894+ramprasadn@users.noreply.github.com>
2023-05-30 17:42:21 +0200

    test

--- recipes/perl-retroseq/build.sh
+++ recipes/perl-retroseq/build.sh
@@ -1,9 +1,8 @@
 #!/bin/bash
 
 # Move the Perl modules to the appropriate 
-export PERL5LIB=/$PREFIX/bin/:$PERL5LIB
-cp ./RetroSeq/Vcf.pm $PREFIX/bin/
-cp ./RetroSeq/Utilities.pm $PREFIX/bin/
+# export PERL5LIB=/$PREFIX/bin/:$PERL5LIB
+cp -r ./RetroSeq/ $PREFIX/
 cp ./bin/retroseq.pl $PREFIX/bin/retroseq.pl
 
 # Set executable permissions for the code.pl file
