755175a233cb39e7f211f49d53eff187a70d3cd5
Ramprasad Neethiraj <20065894+ramprasadn@users.noreply.github.com>
2024-02-22 22:13:50 +0100

    setuptools

--- recipes/gatk4/build_main.sh
+++ recipes/gatk4/build_main.sh
@@ -6,19 +6,10 @@ PACKAGE_HOME=$PREFIX/share/$PKG_NAME-$PKG_VERSION-$PKG_BUILDNUM
 mkdir -p $PREFIX/bin
 mkdir -p $PACKAGE_HOME
 
-git clone https://github.com/broadinstitute/gatk.git gatk-${PKG_VERSION}
-cd gatk-${PKG_VERSION}
-git lfs install --force
-git lfs pull --include src/main/resources/large
-
+#sed -i.bak 's#!/usr/bin/env python#!/opt/anaconda1anaconda2anaconda3/bin/python#' gatk
 chmod +x gatk
 cp gatk ${PACKAGE_HOME}/gatk
-./gradlew localJar
 cp gatk-*-local.jar $PACKAGE_HOME
-cd src/main/python/org/broadinstitute/hellbender/
-
-python setup_gcnvkernel.py install
-python setup.py install
 # Does not install the spark jars, this is done in the `build_spark.sh`
 
 ln -s $PACKAGE_HOME/gatk $PREFIX/bin
