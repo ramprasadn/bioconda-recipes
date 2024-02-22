2cc8e32cde254d3e3e166c497e2f88f6c8ecc38a
Ramprasad Neethiraj <20065894+ramprasadn@users.noreply.github.com>
2024-02-22 10:19:16 +0100

    add setuptools

--- recipes/gatk4/build_main.sh
+++ recipes/gatk4/build_main.sh
@@ -6,10 +6,13 @@ PACKAGE_HOME=$PREFIX/share/$PKG_NAME-$PKG_VERSION-$PKG_BUILDNUM
 mkdir -p $PREFIX/bin
 mkdir -p $PACKAGE_HOME
 
-#sed -i.bak 's#!/usr/bin/env python#!/opt/anaconda1anaconda2anaconda3/bin/python#' gatk
 chmod +x gatk
 cp gatk ${PACKAGE_HOME}/gatk
 cp gatk-*-local.jar $PACKAGE_HOME
+cd src/main/python/org/broadinstitute/hellbender/
+
+python setup_gcnvkernel.py install
+python setup.py install
 # Does not install the spark jars, this is done in the `build_spark.sh`
 
 ln -s $PACKAGE_HOME/gatk $PREFIX/bin
