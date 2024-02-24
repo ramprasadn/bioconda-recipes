ed03393636a52ee035921e592c10559c1ec99982
Ramprasad Neethiraj <20065894+ramprasadn@users.noreply.github.com>
2024-02-25 00:36:51 +0100

    c

--- recipes/gatk4/build_main.sh
+++ recipes/gatk4/build_main.sh
@@ -8,6 +8,12 @@ mkdir -p $PACKAGE_HOME
 
 export PYTHONPATH=$PYTHONPATH:$PREFIX/lib/python3.6/site-packages/
 #sed -i.bak 's#!/usr/bin/env python#!/opt/anaconda1anaconda2anaconda3/bin/python#' gatk
+# pip install --ignore-installed numpy==1.17.5
+# pip install scipy==1.0.0
+# pip install pymc3==3.1
+# pip install Theano==1.0.4
+# pip install PyVCF==0.6.8
+
 chmod +x gatk
 cp gatk ${PACKAGE_HOME}/gatk
 cp gatk-*-local.jar $PACKAGE_HOME
