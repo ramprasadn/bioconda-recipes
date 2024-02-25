54e4691282ffb99cefdc2965c9331d182bf0561c
Ramprasad Neethiraj <20065894+ramprasadn@users.noreply.github.com>
2024-02-25 01:51:29 +0100

    g

--- recipes/gatk4/build_main.sh
+++ recipes/gatk4/build_main.sh
@@ -6,7 +6,7 @@ PACKAGE_HOME=$PREFIX/share/$PKG_NAME-$PKG_VERSION-$PKG_BUILDNUM
 mkdir -p $PREFIX/bin
 mkdir -p $PACKAGE_HOME
 
-export PYTHONPATH=$PYTHONPATH:$PREFIX/lib/python3.6/site-packages/
+#export PYTHONPATH=$PYTHONPATH:$PREFIX/lib/python3.6/site-packages/
 #sed -i.bak 's#!/usr/bin/env python#!/opt/anaconda1anaconda2anaconda3/bin/python#' gatk
 # pip install --ignore-installed numpy==1.17.5
 # pip install scipy==1.0.0
@@ -18,9 +18,9 @@ chmod +x gatk
 cp gatk ${PACKAGE_HOME}/gatk
 cp gatk-*-local.jar $PACKAGE_HOME
 
-unzip gatkPythonPackageArchive.zip -d gatkPythonPackageArchive
-cd gatkPythonPackageArchive
-python setup.py install
+# unzip gatkPythonPackageArchive.zip -d gatkPythonPackageArchive
+# cd gatkPythonPackageArchive
+# python setup.py install
 
 # Does not install the spark jars, this is done in the `build_spark.sh`
 ln -s $PACKAGE_HOME/gatk $PREFIX/bin
